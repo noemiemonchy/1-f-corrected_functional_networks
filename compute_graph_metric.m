a62beb364ea2c113419004dfecfb1af3d24377e6
JD_stimpark_pc <55353429+jduprez@users.noreply.github.com>
2022-12-09 14:59:19 +0100

    Added degree distribution

--- compute_graph_metric.m
+++ compute_graph_metric.m
@@ -119,31 +119,45 @@ graph_t = rmmissing(t);
 writetable(graph_t, [outpath, '/graph_table.csv']);
 
 
-% Create a nsub*gp*thresh*fc*freq*68 matrix with condition specific
+% Create a nsub*gp (1 = HC, 2 = PD) *thresh (1=node, 2 = node_1f)*
+% fc (1 ='plv', 2='wpli', 3='ciplv', 4='oenv', 5='henv')* 
+% freq (1 = delta, 2=theta, 3=alpha, 4=beta, 5=gamma)
+% *68 matrix with condition specific
 % degrees, to investigate degree distribution
 
+degree_dist = zeros(30, 2, 2, 5, 5, 68);
 
+for subi = 1 : size(thr_fc_HC.thresh_node_mats, 1)
+    for fci = 1 : size(thr_fc_HC.thresh_node_mats, 2)
+        for freqi = 1 : size(thr_fc_HC.thresh_node_mats, 5)
+            degree_dist(subi, 1, 1, fci, freqi, :) = degrees_und(squeeze(thr_fc_HC.thresh_node_mats(subi,fci,:,:,freqi)));
+        end
+    end
+end
+% thresh node 1/f
+for subi = 1 : size(thr_fc_1f_HC.thresh_node_1f_mats, 1)
+    for fci = 1 : size(thr_fc_1f_HC.thresh_node_1f_mats, 2)
+        for freqi = 1 : size(thr_fc_1f_HC.thresh_node_1f_mats, 5)
+            degree_dist(subi, 1, 2, fci, freqi, :) = degrees_und(squeeze(thr_fc_1f_HC.thresh_node_1f_mats(subi,fci,:,:,freqi)));
+        end
+    end
+end
+% PD
+% thresh node
+for subi = [1 : 7, 9: size(thr_fc_PD.thresh_node_mats, 1)]
+    for fci = 1 : size(thr_fc_PD.thresh_node_mats, 2)
+        for freqi = 1 : size(thr_fc_PD.thresh_node_mats, 5)
+            degree_dist(subi, 2, 1, fci, freqi, :) = degrees_und(squeeze(thr_fc_PD.thresh_node_mats(subi,fci,:,:,freqi)));
+        end
+    end
+end
+% thresh node 1/f
+for subi = [1 : 7, 9:size(thr_fc_1f_PD.thresh_node_1f_mats, 1)]
+    for fci = 1 : size(thr_fc_1f_PD.thresh_node_1f_mats, 2)
+        for freqi = 1 : size(thr_fc_1f_PD.thresh_node_1f_mats, 5)
+            degree_dist(subi, 2, 2, fci, freqi, :) = degrees_und(squeeze(thr_fc_1f_PD.thresh_node_1f_mats(subi,fci,:,:,freqi)));
+        end
+    end
+end
 
-% degree
-
-deg = degrees_und(squeeze(thr_fc_HC.thresh_node_mats(1,1,:,:,4)));
-deg2 = degrees_und(squeeze(thr_fc_1f_HC.thresh_node_1f_mats(1,1,:,:,4)));
-
-% strength
-
-strength = strengths_und(squeeze(thr_fc_HC.thresh_node_mats(1,1,:,:,4)));
-strength2 = strengths_und(squeeze(thr_fc_1f_HC.thresh_node_1f_mats(1,1,:,:,4)));
-
-% clustering coefficient
-
-clu = clustering_coef_wu(squeeze(thr_fc_HC.thresh_node_mats(1,1,:,:,4)));
-clu2 = clustering_coef_wu(squeeze(thr_fc_1f_HC.thresh_node_1f_mats(1,1,:,:,4)));
-
-% path length
-path = charpath(squeeze(thr_fc_HC.thresh_node_mats(1,1,:,:,5)));
-path2 = charpath(squeeze(thr_fc_1f_HC.thresh_node_1f_mats(1,1,:,:,5)));
-
-% betweenness centrality
-betw = betweenness_wei(squeeze(thr_fc_HC.thresh_node_mats(1,1,:,:,3)));
-betw2 = betweenness_wei(squeeze(thr_fc_1f_HC.thresh_node_1f_mats(1,1,:,:,3)));
-% Graph distance ?
+save('degree_dist.mat', 'degree_dist');
