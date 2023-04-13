6d655baced1d9f58bee34c4addeb29f3878df774
JD_stimpark_pc <55353429+jduprez@users.noreply.github.com>
2023-04-13 16:50:16 +0200

    change structure subfields

--- Get_specparam_aperio.m
+++ Get_specparam_aperio.m
@@ -11,8 +11,14 @@ for subi = [1 :nsub]
     subpath = dir('sub*');
     cd(subpath(1).name)
     specpath = dir('*specparam*.mat');
-    temp = load(specpath.name);
-    specparam_hc(subi) = temp.Options.FOOOF;
+    temp_source = load(specpath(1).name);
+    temp_chan = load(specpath(2).name);
+    specparam_source_hc(subi).spec = temp_source.Options.FOOOF;
+    specparam_source_hc(subi).sub = subpath(1).name(1:7);
+    specparam_source_hc(subi).group = 'HC';    
+    specparam_scalp_hc(subi).spec = temp_chan.Options.FOOOF;
+    specparam_scalp_hc(subi).sub = subpath(1).name(1:7);
+    specparam_scalp_hc(subi).group = 'HC';    
     cd ..
     cd ..
 end
@@ -28,8 +34,16 @@ for subi = [1 : 7, 9:nsub]
     subpath = dir('sub*');
     cd(subpath(1).name)
     specpath = dir('*specparam*.mat');
-    temp = load(specpath.name);
-    specparam_pd(subi) = temp.Options.FOOOF;
+    temp_source = load(specpath(1).name);
+    temp_chan = load(specpath(2).name);
+    specparam_source_pd(subi).spec = temp_source.Options.FOOOF;
+    specparam_source_pd(subi).sub = subpath(1).name(1:7);
+    specparam_source_pd(subi).group = 'PD';    
+    specparam_scalp_pd(subi).spec = temp_chan.Options.FOOOF;
+    specparam_scalp_pd(subi).sub = subpath(1).name(1:7);
+    specparam_scalp_pd(subi).group = 'PD';    
     cd ..
     cd ..
 end
+
+specparam_source_pd(8) = [];
