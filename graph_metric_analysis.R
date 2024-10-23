8a7f3abed0e618d30c738c824ab0ded2df3ab931
noemiemonchy <127295493+noemiemonchy@users.noreply.github.com>
2024-10-23 14:06:53 +0200

    Update graph_metric_analysis.R

--- graph_metric_analysis.R
+++ graph_metric_analysis.R
@@ -2,6 +2,19 @@ library(tidyverse)
 library(ggpubr)
 library(see)
 library(ggbeeswarm)
+library(forcats)
+library(dplyr)
+library(cowplot)
+library(rstatix)
+library(ggsci)
+library(MASS)
+library(lme4)
+
+
+#####################################################
+####################### PLOTS #######################
+#####################################################
+
 
 ## Plot n nodes of kept  after 1/f thresh
 
@@ -138,4 +151,157 @@ p <- data %>%
   guides(fill = guide_legend(override.aes = list(alpha = 1,color="black")))+
   ggtitle("5% proportional + 1/f")+
   ylim(0, 175)
+
+
+#####################################################
+####################### STATS #######################
+#####################################################
+
+########### STATS DATASET A
+
+# GRAPH METRIC STATS - (gp x thresh_met) -------------------------------------
+
+data = as_tibble(read.table('graph_table.csv', 
+                            sep = ";", header = TRUE))
+
+data$frequencies = as.factor(data$frequencies)
+data$thresh_met = as.factor(data$thresh_met)
+data$sub = as.factor(data$sub)
+data = data[which(data$fc_meth == "plv"),]
+data = data[which(data$frequencies == "alpha"),]
+
+### STATS -------------------------------------------------------------------
+
+# Stats
+data2 <- data %>%
+  group_by(sub, group, thresh_met) %>% 
+  convert_as_factor(sub, thresh_met, group)
+
+# Descriptive stats
+data2 %>%
+  group_by(thresh_met, group) %>%
+  get_summary_stats(path, type = "mean_sd")
+
+# Identify outliers
+data2 %>%
+  group_by(thresh_met, group) %>%
+  identify_outliers(path)
+
+######## 2 assumptions for repeated measures ANOVA ####################
+# 1st one : the data at each time point are approximately normally distributed
+
+# Normality check
+data2 %>%
+  group_by(thresh_met, group) %>%
+  shapiro_test(path)
+
+ggqqplot(data2, "path", ggtheme = theme_bw()) +
+  facet_grid(thresh_met ~ group, labeller = "label_both")
+
+# 2nd point : sphericity = the variances of the differences between all combinations
+# of the related conditions points are equal 
+
+# Calculate anova
+data2 = as.data.frame(data2)
+res.aov <- anova_test(
+  data = data2, dv = path, wid = sub,
+  between = group, within = thresh_met
+)
+res.aov
+
+get_anova_table(res.aov, correction = c("auto"))
+
+
+metric_results = matrix(nrow=1, ncol=6, byrow=TRUE)
+
+# specify the column names and row names of matrix
+colnames(metric_results) = c('F_group','F_threshmet', 'F_interaction','p_group','p_threshmet','p_inter')
+
+# # assign to table
+#expo_results = as.table(expo_results)
+
+metric_results[1,1] = res.aov$F[1]
+metric_results[1,2] = res.aov$F[2]
+metric_results[1,3] = res.aov$F[3]
+metric_results[1,4] = res.aov$p[1]
+metric_results[1,5] = res.aov$p[2]
+metric_results[1,6] = res.aov$p[3]
+
+# import the table with all p-values for each method in order to apply the FDR correction 
+
+p = read.table("pval_plv_graph.csv", sep=";",header=F)
+p1 = p[,1]
+p = as.numeric(unlist(p))
+a = p.adjust(p,method="fdr",n=length(p))
+
+########### STATS DATASET B
+
+# GRAPH METRIC STATS - (thresholding effect) -------------------------------------
+
+data = as_tibble(read.table('graph_table_SRM.csv', 
+                            sep = ",", header = TRUE))
+
+data$frequencies = as.factor(data$frequencies)
+data$thresh_met = as.factor(data$thresh_met)
+data$sub = as.factor(data$sub)
+data = data[which(data$fc_meth == "plv"),]
+data = data[which(data$frequencies == "alpha"),]
+
+### STATS -------------------------------------------------------------------
+
+# Stats
+data2 <- data %>%
+  group_by(sub, thresh_met) %>% 
+  convert_as_factor(sub, thresh_met)
+
+# Descriptive stats
+data2 %>%
+  group_by(thresh_met) %>%
+  get_summary_stats(mean_clustering, type = "mean_sd")
+
+# Identify outliers
+data2 %>%
+  group_by(thresh_met) %>%
+  identify_outliers(mean_clustering)
+
+### PAIRED T-TEST STUDENT
+
+stat.test <- data2  %>% 
+  t_test(mean_clustering ~ thresh_met, paired = TRUE) %>%
+  add_significance()
+stat.test
+
+size =  data2  %>% cohens_d(mean_clustering ~ thresh_met, paired = TRUE)
+
+metric_results = matrix(nrow=1, ncol=5, byrow=TRUE)
+
+# specify the column names and row names of matrix
+colnames(metric_results) = c('T','df', 'p_threshmet','effsize','magnitude')
+
+# # assign to table
+#expo_results = as.table(expo_results)
+
+metric_results[1,1] = stat.test$statistic
+metric_results[1,2] = stat.test$df
+metric_results[1,3] = stat.test$p
+metric_results[1,4] = size$effsize
+metric_results[1,5] = size$magnitude
+
+write.csv(metric_results, "mean_clustering_alpha_SRM.csv", row.names=TRUE)
+
+# import the table with all p-values for each method in order to apply the FDR correction 
+p = read.table("pval_SRM_PLV_r.csv", sep=",",header=F)
+
+p1 = p[,1]
+
+p = as.numeric(unlist(p))
+a = p.adjust(p,method="fdr",n=length(p))
+
+
+write.csv(a, "pvalc_PLV_graph_metrics_SRM.csv", row.names=TRUE)
+
+
+
+
+
 
