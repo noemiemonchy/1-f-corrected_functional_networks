29dc35d8efacb9e6453aa8152d3e8f40259d1128
noemiemonchy <127295493+noemiemonchy@users.noreply.github.com>
2024-11-06 15:49:46 +0100

    Update threshold_fc_matrices.m

--- threshold_fc_matrices.m
+++ threshold_fc_matrices.m
@@ -25,29 +25,10 @@ for subi = 1:size(thresh_node_mats, 1)
             
             temp_fc_mat = squeeze(result_mat(subi, fci,:,:,freqi));
             thresh_node_mats(subi, fci,:,:,freqi) = threshold_proportional(temp_fc_mat, 0.05);
-            temp_fc_mat_4_1f = threshold_proportional(temp_fc_mat, 0.05);
             
-            
-            % Now threshold further using specparam results
-            temp_thresh_mat = squeeze(new_spec_mat(subi, freqi,:,:));
-            
-            temp_fc_mat_4_1f(temp_thresh_mat == 0) = 0;
-            
-            thresh_node_1f_mats(subi, fci,:,:,freqi) = temp_fc_mat_4_1f;
             
         end
     end
     
-end
-
-
-
-
-
-for freqi = 1:5
-    figure
-    imagesc(squeeze(mean(thresh_node_1f_mats(:,5,:,:,freqi),1)))
-    set(gca,'clim',[0,1])
-    colorbar
 end
 
